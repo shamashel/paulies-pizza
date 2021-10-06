7003e5a51187a9670f77cefc5811195c24b8068d
shamashel <root@DESKTOP-K5527AM.localdomain>
2021-10-06 18:59:27 -0400

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -9,8 +9,26 @@
 </template>
 
 <style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Libre+Baskerville:wght@400;700&display=swap');
+:root {
+  --primary: #C40815;
+  --bs-primary: var(--primary) !important;
+  --primaryHover: #C92F3A;
+  --black: #222222;
+  --white: #FFFFFF;
+  --muted: #5E5E5E;
+  --neutral-0: #f7f5f0;
+  --neutral-1: #FBF7EE;
+  --neutral-2: #EED7C8;
+  --neutral-3: #DFD5CF;
+  --linkHover: #EFEFF2;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Libre Baskerville', serif;
+  background-color: var(--neutral-1);
+  position: relative;
+  min-height: 100vh;
+  height: 100%;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/components/Header.vue
@@ -0,0 +1,62 @@
+<template>
+  <header class="header">
+    <div class="container">
+      <div class="row">
+        <img src="../assets/Logo.svg" class="header-logo mb-4 col-12 col-md-3 align-self-center" />
+        <div class="header-navbar container mb-4 col-12 col-md-6 align-self-center">
+          <div class="row">
+            <!-- Render each of the routes -->
+            <div class="col" v-for="{name, route} in routes" v-bind:key="route">
+              <a class="btn hover" v-bind:href="route">{{ name }}</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-3 mb-4 align-self-center d-none d-md-flex d-lg-flex d-xl-flex">
+          <div class="header-slogan">The fastest, freshest Pizza in town, straight to your door</div>
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+  export default {
+    name: 'Header',
+    data() {
+      return {
+        routes: [
+          { name: 'Home', route: '/home' },
+          { name: 'Order', route: '/order' },
+          { name: 'Tracking', route: '/tracking' },
+          { name: 'Business', route: '/about' },
+        ],
+      };
+    },
+  };
+</script>
+
+<style scoped>
+  .header {
+    width: 100vw;
+    padding-top: 10px;
+    background-color: var(--white);
+    box-shadow: 0 0 0 1px rgb(67 41 163 / 10%), 0 1px 8px 0 rgb(67 41 163 / 10%);
+  }
+  .header-logo {
+    max-height: 55px;
+  }
+
+  .header-navbar {
+    text-align: center;
+    font-size: 14px !important;
+  }
+  .header-slogan {  
+    font-style: normal;
+    font-weight: normal;
+    max-width: 15ch;
+    font-size: 12px;
+    line-height: 15px;
+    text-align: center;
+    color: var(--muted);
+  }
+</style>
