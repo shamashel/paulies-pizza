8e13626e51de85dd8ca9386d340258f899c6c9a3
Michael Gabriel <michaelgabrielinfosec@gmail.com>
2021-10-07 18:07:40 -0400

    Add deploy script

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const bootstrapSassAbstractsImports = require('vue-cli-plugin-bootstrap-vue/sassAbstractsImports.js')
 module.exports = {
+	publicPath: '/paulies-pizza',
 	css: {
 		loaderOptions: {
 			sass: {
