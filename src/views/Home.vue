8d5c5964b98f682bc4ce77be499f20f9faa31bc3
Michael Gabriel <michaelgabrielinfosec@gmail.com>
2021-10-17 18:54:02 -0400

    Add content to home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,24 +1,41 @@
 <template>
-  <div class="home">
-    <div class="container px-4 px-lg-5 mt-5">
-      <div class="jumbotron">
-        <h1>Welcome to Paulie's Pizza!</h1>
-        <!-- Main Descriptive content here -->
-        <!-- Should consist of a general explanation of the business with a call to action to place an order -->
-        <div class="landing-content">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci dignissimos saepe consectetur eos, harum molestias officia eaque explicabo, vero sapiente culpa. Ipsum accusantium in error libero dolores ex quia cupiditate!
-        </div>
-        <hr>
-        <p>For business inquiries, contact us <router-link class="link-primary" to="/contact">here</router-link></p>
-        <router-link class="btn btn-primary" to="/order">Start an Order</router-link>
+  <div class="home container h-100 px-4 px-lg-5 mt-5">
+    <div class="jumbotron align-self-center">
+      <h1>Welcome to Paulie's Pizza!</h1>
+      <hr>
+      <!-- Main Descriptive content here -->
+      <!-- Should consist of a general explanation of the business with a call to action to place an order -->
+      <div class="landing-content">
+        <p>
+          Paulie’s Pizza seeks to take the world of classic Italian food into the modern day of convenience and speed!
+        </p>
+        <p>
+          We work together with businesses that might not normally be able to handle online delivery, and provide them with the
+          recipes and infrastructure to start selling locally.
+        </p>
+        <p>
+          If you're a hungry customer, that means you can quickly start an order and get great food delivered to you
+          entirely online while supporting local businesses!
+        </p>
       </div>
+      <router-link class="btn btn-primary" to="/order">Start an Order</router-link>
+      <hr>
+      <p>For business inquiries, contact us <router-link class="link-primary" to="/contact">here</router-link></p>
     </div>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'Home',
-  components: {}
-}
+  export default {
+    name: 'Home',
+    components: {}
+  }
 </script>
+<style lang="scss" scoped>
+  .landing-content {
+    margin: 10px 0;
+  }
+  .jumbotron {
+    background-color: var(--neutral-0);
+  }
+</style>
